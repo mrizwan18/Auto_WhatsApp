16f36f5f99d234eedffa4263f01318179eea2858
Razi <razibhatti1@gmail.com>
2019-08-25 18:10:43 +0500

    initial

---
+++ whatsapp_messaging.py
@@ -0,0 +1,46 @@
+import praw
+from twilio.rest import Client
+
+reddit = praw.Reddit(client_id='NI8XvqPAjb31Zw', client_secret='-cBaigpGIUOGmoSFcq6CfEQsWsU', user_agent='AutoWhatsapp')
+posts = []
+subreddits = ['oddlysatisfying']
+
+
+def scrap_reddit():
+    for subreddit in subreddits:
+        hot_posts = reddit.subreddit(subreddit).hot(limit=10)
+        for post in hot_posts:
+            posts.append(post.url)
+
+
+def send_message(event=None, context=None):
+    try:
+        scrap_reddit()
+    except:
+        print("Something went wrong")
+    # sid and auth token from twilio
+    twilio_sid = 'AC91e2e304f48902f052e47d23a5ee894b'
+    auth_token = '13bc910c663fff31ec20b3bd28ad5bd9'
+    try:
+        whatsapp_client = Client(twilio_sid, auth_token)
+    except:
+        print("Something went wrong")
+
+    # Contact list to send messages to
+    contact_directory = {'Hamza Anjum': '+923206080396'}
+
+    for key, value in contact_directory.items():
+        for post in posts:
+            try:
+                message = whatsapp_client.messages.create(
+                    body='Top Post from *Oddly Satisfying*: {} !'.format(post),
+                    from_='whatsapp:+14155238886',
+                    to='whatsapp:' + value,
+                )
+                print(message.body)
+            except Exception as e:
+                print(e)
+
+
+if __name__ == "__main__":
+    send_message()
